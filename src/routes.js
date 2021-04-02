195c20aa028184c46e89b153a84c3431918a891f
Greg Lixandrão <contato@greglixandrao.com>
2021-04-01 21:04:15 -0300

    Adicionando funcionalidades a parte de adicionar jobs
    transformando os input em objeto e adicionando a um array, usando urlencoded no express, recebendo dados do frontend no backend

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,36 @@
-const express = require("express");
+const express = require('express');
 const routes = express.Router();
 
-const views = __dirname + "/views/";
+const views = __dirname + '/views/';
 
 const profile = {
-  name: "Greg Lixandrão",
-  avatar: "https://avatars.githubusercontent.com/u/13009208?v=4",
-  "monthly-budget": 2000,
-  "days-per-week": 5,
-  "hours-per-day": 4,
-  "vacation-per-year": 4,
+  name: 'Greg Lixandrão',
+  avatar: 'https://avatars.githubusercontent.com/u/13009208?v=4',
+  'monthly-budget': 2000,
+  'days-per-week': 5,
+  'hours-per-day': 4,
+  'vacation-per-year': 4,
 };
 
-routes.get("/", (req, res) => res.render(views + "index"));
+const jobs = [];
 
-routes.get("/job", (req, res) => res.render(views + "job"));
+routes.get('/', (req, res) => res.render(views + 'index'));
 
-routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
+routes.get('/job', (req, res) => res.render(views + 'job'));
 
-routes.get("/profile", (req, res) =>
-  res.render(views + "profile", { profile })
+routes.post('/job', (req, res) => {
+  jobs.push(req.body);
+  return res.redirect('/');
+});
+
+routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
+
+routes.get('/profile', (req, res) =>
+  res.render(views + 'profile', { profile })
 );
 
-routes.get("/index.html", (req, res) => {
-  return res.redirect("/");
+routes.get('/index.html', (req, res) => {
+  return res.redirect('/');
 });
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ server.set("view engine", "ejs");
 // habilitar arquivo statics
 server.use(express.static("public"));
 
+// usar o req.body
+server.use(express.urlencoded({ extended: true }));
+
 // rotas
 server.use(routes);
 
