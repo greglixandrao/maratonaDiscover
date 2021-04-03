e61061d451887550b580e37dcec57e370d0474ea
Greg Lixandrão <contato@greglixandrao.com>
2021-04-03 13:54:45 -0300

    Implementação nas rotas de cálculos de dias restante
    Implementação das funções de cálculo de dias restantes e refatoração do index para implementar os conteúdos dinamicos

--- src/routes.js
+++ src/routes.js
@@ -10,16 +10,70 @@ const profile = {
   'days-per-week': 5,
   'hours-per-day': 4,
   'vacation-per-year': 4,
+  'value-hours': 75,
 };
 
-const jobs = [];
+const jobs = [
+  {
+    id: 1,
+    name: 'Pizzaria Guloso',
+    'daily-hours': 2,
+    'total-hours': 1,
+    createdAt: Date.now(),
+  },
+  {
+    id: 2,
+    name: 'OneTwo Project',
+    'daily-hours': 3,
+    'total-hours': 47,
+    createdAt: Date.now(),
+  },
+];
 
-routes.get('/', (req, res) => res.render(views + 'index'));
+function remainingDays(job) {
+  // calculo de tempo restante
+  const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed();
+
+  const createdDate = new Date(job.createdAt);
+  const dueDay = createdDate.getDate() + Number(remainingDays);
+  const dueDateInMs = createdDate.setDate(dueDay); // Data futura de entrega no job
+  const timeDiffInMs = dueDateInMs - Date.now();
+
+  // transformar milli em dias
+  const dayInMs = 1000 * 60 * 60 * 24;
+  const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+  // restam x dias
+  return dayDiff;
+}
+
+routes.get('/', (req, res) => {
+  const updateJobs = jobs.map((job) => {
+    // ajustes nos jobs
+    const remaining = remainingDays(job);
+    const status = remaining <= 0 ? 'done' : 'progress';
+    return {
+      ...job,
+      remaining,
+      status,
+      budget: profile['value-hours'] * job['total-hours'],
+    };
+  });
+
+  return res.render(views + 'index', { jobs: updateJobs });
+});
 
 routes.get('/job', (req, res) => res.render(views + 'job'));
 
 routes.post('/job', (req, res) => {
-  jobs.push(req.body);
+  const lastId = jobs[jobs.length - 1]?.id || 1;
+
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    'daily-hours': req.body['daily-hours'],
+    'total-hours': req.body['total-hours'],
+    createdAt: Date.now(), // atribuindo a data de hoje
+  });
   return res.redirect('/');
 });
 
