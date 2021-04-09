2e303635a1d770f3443f408a7cd311eaec376635
Greg Lixandrão <contato@greglixandrao.com>
2021-04-09 10:57:24 -0300

    Finalização Aula 04 Refatoração para BD de Jobs
    Refatoração do CRUD de Jobs

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,15 +1,5 @@
 const Database = require('../db/config');
 
-// let data = {
-//   name: 'Greg Lixandrão',
-//   avatar: 'https://avatars.githubusercontent.com/u/13009208?v=4',
-//   'monthly-budget': 2000,
-//   'days-per-week': 5,
-//   'hours-per-day': 4,
-//   'vacation-per-year': 4,
-//   'value-hour': 70,
-// };
-
 module.exports = {
   async get() {
     const db = await Database();
